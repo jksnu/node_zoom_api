9721c3ca0c93fc4082f75da4ef955fe873027f1f
Jitendra Kumar Singh <singh.jitendra_kr@rediffmail.com>
2021-03-18 19:26:13 +0530

    adding correct config

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ const env = process.env.NODE_ENV || 'production'
 //insert your API Key & Secret for each environment, keep this file local and never push it to a public repo for security purposes.
 const config = {
 	development :{
-		APIKey : 'sLYP34EXQ7SDS-PueWck_w',
-		APISecret : 'bxAQCsho7DpnFj9OliNoPyDjbEts5tDPKYfu'
+		APIKey : 'Your API KEY',
+		APISecret : 'Your API Secret Key'
 	},
 	production:{	
-		APIKey : 'sLYP34EXQ7SDS-PueWck_w',
-		APISecret : 'bxAQCsho7DpnFj9OliNoPyDjbEts5tDPKYfu'
+		APIKey : 'Your API KEY',
+		APISecret : 'Your API Secret Key'
 	}
 };
 
